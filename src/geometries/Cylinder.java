f223d971081d7b30df4475540208d77f41bd5525
Bells333 <155893247+Bells333@users.noreply.github.com>
2024-01-10 22:57:37 +0200

    Create Cylinder.java

---
+++ src/geometries/Cylinder.java
@@ -0,0 +1,17 @@
+package geometries;
+import primitives.Point;
+import primitives.Vector;
+
+public class Cylinder extends Tube{
+	
+	public Cylinder(double radius) {
+		super(radius);
+		
+	}
+	private double height;
+	public Vector getNormal(Point p)
+	{
+		return null;
+	}
+
+}
