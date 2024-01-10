2263561c4819b68cc212a0dc31d017fec332d2c4
Bells333 <155893247+Bells333@users.noreply.github.com>
2024-01-10 22:37:01 +0200

    Create Triangle.java

---
+++ src/geometries/Triangle.java
@@ -0,0 +1,14 @@
+package geometries;
+import primitives.Point;
+import primitives.Vector;
+
+public class Triangle extends Polygon{
+	
+	public Triangle(Point p1, Point p2, Point p3)
+	{
+		super(p1, p2, p3);
+	}
+public Vector getNormal(Point p)
+{ return super.getNormal(p);
+	}
+}
