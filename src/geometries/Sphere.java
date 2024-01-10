17f6e68a0c330bdcda811cfb97f0cd974e0f618a
Bells333 <155893247+Bells333@users.noreply.github.com>
2024-01-10 22:55:48 +0200

    Create Sphere.java

---
+++ src/geometries/Sphere.java
@@ -0,0 +1,18 @@
+package geometries;
+import primitives.Point;
+import primitives.Vector;
+
+public class Sphere extends RadialGeometry{
+	public Sphere(double radius) {
+		super(radius);
+		this.center = null;
+		
+		
+	}
+	private final Point center;
+	public Vector getNormal(Point p) {
+	return null;
+	}
+	
+
+}
